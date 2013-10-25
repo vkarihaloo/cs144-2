9cce1f6356a38b1802f20f19d68db9e95b4b34c6
Steven <stevenly2@gmail.com>
2013-10-25 01:08:05 -0700

    added categories table, primary and foreign keys

--- project2/create.sql
+++ project2/create.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE Users (
 	UsersID VARCHAR(50) PRIMARY KEY,
 	Location VARCHAR(100),
@@ -14,10 +12,11 @@ CREATE TABLE Items (
 	Currently DECIMAL(8,2), 
 	First_Bid DECIMAL(8,2), 
 	Buy_Price DECIMAL(8,2),
-	Number_of_Bids INT, 
+	Number_of_Bids INT,  
 	Started TIMESTAMP, 
 	Ends TIMESTAMP, 
-	Description VARCHAR(4000)
+	Description VARCHAR(4000),
+	FOREIGN KEY (SellerID) REFERENCES Users (UsersID)
 	);
 
 CREATE TABLE Bids (
@@ -25,5 +24,13 @@ CREATE TABLE Bids (
 	BidderID VARCHAR(50),
 	Time TIMESTAMP,
 	Amount DECIMAL(8,2),
-	CONSTRAINT BidsKey PRIMARY KEY (ItemID, BidderID, Time)
+	CONSTRAINT BidsKey PRIMARY KEY (ItemID, BidderID, Time),
+	FOREIGN KEY (ItemID) REFERENCES Items (ItemID)
+	);
+
+CREATE TABLE Categories (
+	ItemID INT,
+	Category VARCHAR(50),
+	PRIMARY KEY (ITEMID),
+	FOREIGN KEY (ItemID) REFERENCES Items (ItemID)
 	);
