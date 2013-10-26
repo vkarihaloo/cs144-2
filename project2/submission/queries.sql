c01c13807e0d54a506844c4d8475701c064f5517
Steven <stevenly2@gmail.com>
2013-10-25 22:55:49 -0700

    fixed query problem, changed schema for UserID

--- project2/submission/queries.sql
+++ project2/submission/queries.sql
@@ -1,12 +1,12 @@
 SELECT COUNT(*) FROM Users;
 
-SELECT COUNT(*) FROM Users WHERE UsersID in (SELECT SellerID FROM Items) AND BINARY Location = 'New York';
+SELECT COUNT(*) FROM Users WHERE UserID in (SELECT SellerID FROM Items) AND BINARY Location = 'New York';
 
 SELECT COUNT(*) FROM (SELECT ItemID FROM Categories GROUP BY ItemID HAVING COUNT(Category) = 4) A;
 
-SELECT ItemID, Number_of_Bids FROM Items WHERE Currently = (select max(Currently) FROM Items WHERE Ends > '2001-12-20 00:00:01' and Number_of_Bids > 0) and Number_of_Bids > 0;
+SELECT ItemID FROM Items WHERE Currently = (select max(Currently) FROM Items WHERE Ends > '2001-12-20 00:00:01' and Number_of_Bids > 0) and Number_of_Bids > 0;
 
-SELECT COUNT(*) FROM Users WHERE UsersID in (SELECT SellerID FROM Items) AND rating > 1000;
+SELECT COUNT(*) FROM Users WHERE UserID in (SELECT SellerID FROM Items) AND rating > 1000;
 
 SELECT COUNT(*) FROM (SELECT DISTINCT Items.SellerID FROM Items INNER JOIN Bids ON Items.SellerID = Bids.BidderID) A;
 
