16436cb767a06c3fcd4d759b6cb4e1c4e086cea0
Steven <steven@Stevens-Computer.local>
2013-11-01 22:44:10 -0700

    created buildSQLindex.sql and dropSQLIndex.sql files, and zipped folder

---
+++ project3-indexer/buildSQLIndex.sql
@@ -0,0 +1,4 @@
+CREATE INDEX sellerindex ON Items(SellerID);
+CREATE INDEX buypriceindex ON Items(Buy_Price);
+CREATE INDEX bidderindex ON Bids(BidderID);
+CREATE INDEX endtimeindex ON Items(Ends);
---
+++ project3-indexer/submissionA/dropSQLIndex.sql
@@ -0,0 +1,4 @@
+DROP INDEX sellerindex ON Items;
+DROP INDEX buypriceindex ON Items;
+DROP INDEX bidderindex ON Bids;
+DROP INDEX endtimeindex ON Items;
