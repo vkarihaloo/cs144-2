178f1308210d4b873ec65748e633233581ba3064
Steven <steven@Stevens-Computer.local>
2013-11-09 23:02:13 -0800

    added some test cases changed some zip files

--- project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -18,16 +18,23 @@ public class AuctionSearchTest {
 		System.out.println("Reply: " + reply);
 		
 		String query = "superman";
-		SearchResult[] basicResults = as.basicSearch(query, 0, 72);
+		SearchResult[] basicResults = as.basicSearch(query, 20, 72);
 		System.out.println("Basic Search Query: " + query);
 		System.out.println("Received " + basicResults.length + " results");
 		for(SearchResult result : basicResults) {
 			System.out.println(result.getItemId() + ": " + result.getName());
 		}
 		
+		query = "kitchenware";
+		basicResults = as.basicSearch(query, 20, 0);
+		System.out.println("Basic Search Query: " + query);
+		System.out.println("Received " + basicResults.length + " results");
+		/*for(SearchResult result : basicResults) {
+			System.out.println(result.getItemId() + ": " + result.getName());
+		}*/
 		
 		SearchConstraint constraint =
-		    new SearchConstraint(FieldName.BuyPrice, "5.99"); 
+		    new SearchConstraint(FieldName.EndTime, "Dec-14-01 21:00:05"); 
 		SearchConstraint[] constraints = {constraint};
 		SearchResult[] advancedResults = as.advancedSearch(constraints, 0, 20);
 		System.out.println("Advanced Seacrh");
@@ -35,9 +42,26 @@ public class AuctionSearchTest {
 		for(SearchResult result : advancedResults) {
 			System.out.println(result.getItemId() + ": " + result.getName());
 		}
+
+		constraint =
+		    new SearchConstraint(FieldName.ItemName, "Precious Moments");
+		SearchConstraint constraint2 =
+		    new SearchConstraint(FieldName.SellerId, "waltera317a"); 
+		constraints = new SearchConstraint[]{constraint, constraint2};
+		advancedResults = as.advancedSearch(constraints, 0, 20);
+		System.out.println("Advanced Seacrh");
+		System.out.println("Received " + advancedResults.length + " results");
+		for(SearchResult result : advancedResults) {
+			System.out.println(result.getItemId() + ": " + result.getName());
+		}
 		
-		String itemId = "1497497054";
+		String itemId = "1497595357";
 		String item = as.getXMLDataForItemId(itemId);
+		System.out.println("XML data for ItemId: " + itemId);
+		System.out.println(item);
+
+		itemId = "1679249851";
+		item = as.getXMLDataForItemId(itemId);
 		System.out.println("XML data for ItemId: " + itemId);
 		System.out.println(item);
 		// Add your own test here
